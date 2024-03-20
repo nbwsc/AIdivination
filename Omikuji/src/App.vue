1eefd901a43c41522797db725639760bbde6ea90
shichao.wang <shichao.wang@leapcapital.com>
2024-03-20 11:18:14 +0800

    feat: Omikuji update

---
+++ Omikuji/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Input from './components/Input.vue'
+</script>
+
+<template>
+  <Input />
+</template>
+
+<style scoped>
+</style>
---
+++ Omikuji/src/i18n.js
@@ -0,0 +1,30 @@
+export default {
+    zh: {
+        lang: '中文',
+        title: '御神签',
+        ph_fortunes: "请输入您的签文",
+        ph_poems: "请输入您的诗句",
+        ph_submit: "提交",
+    },
+    en: {
+        lang: 'English',
+        title: 'Fortune Stick',
+        ph_fortunes: "Enter your fortune",
+        ph_poems: "Enter your poem",
+        ph_submit: "Submit",
+    },
+    ja: {
+        lang: '日本語',
+        title: 'おみくじ',
+        ph_fortunes: "おみくじを入力してください",
+        ph_poems: "詩を入力してください",
+        ph_submit: "提出する",
+    },
+    ko: {
+        lang: '한국어',
+        title: '운세',
+        ph_fortunes: "운세를 입력하세요",
+        ph_poems: "시를 입력하세요",
+        ph_submit: "제출",
+    },
+}
