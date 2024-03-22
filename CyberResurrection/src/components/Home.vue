efa45704325a78912e3b8b83c0f3bf646cfe3aea
shichao.wang <shichao.wang@leapcapital.com>
2024-03-22 18:41:35 +0800

    [auto-merge-wsc_macpro]

---
+++ CyberResurrection/src/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    Home page
+</template>
+<script lang="ts" setup>
+
+</script>
---
+++ CyberResurrection/src/i18n.js
@@ -0,0 +1,39 @@
+export default {
+  English: {
+    lang: "English",
+    title: "Fortune Stick",
+    ph_fortunes: "Enter your fortune",
+    ph_poems: "Enter your poem",
+    submit: "Submit",
+    again: "Draw Again",
+    empty: "Please enter the fortune and poem.",
+  },
+  Japanese: {
+    lang: "日本語",
+    title: "おみくじ",
+    ph_fortunes: "おみくじを入力してください",
+    ph_poems: "詩を入力してください",
+    submit: "提出する",
+    again: "もう一度引く",
+    empty: "運勢と詩を入力してください。",
+  },
+
+  Chinese: {
+    lang: "中文",
+    title: "御神签",
+    ph_fortunes: "请输入您的签文",
+    ph_poems: "请输入您的诗句",
+    submit: "提交",
+    again: "再抽一签",
+    empty: "请输入您的签文和诗句。",
+  },
+  Korean: {
+    lang: "한국어",
+    title: "운세",
+    ph_fortunes: "운세를 입력하세요",
+    ph_poems: "시를 입력하세요",
+    submit: "제출",
+    again: "다시 뽑기",
+    empty: "운세와 시를 입력하세요.",
+  },
+};
