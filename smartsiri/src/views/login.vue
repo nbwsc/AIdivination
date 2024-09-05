8e8457c1f4513a37d0caa38b028e44c5222e762f
Shichao.Wang <nbwsc@icloud.com>
2024-09-05 15:20:01 +0800

    [auto-merge-wsc_macpro]

--- smartsiri/src/views/login.vue
+++ smartsiri/src/views/login.vue
@@ -22,8 +22,8 @@
                     @click="gotomodel">免费下载模型</el-button>
                 <qrcode />
             </form>
-            <div style="text-align: center;position: absolute;bottom: 20px;">
-                <a href="https://www.samr.gov.cn/ ">京ICP备19019954号</a>
+            <div style="text-align: center;position: absolute;bottom: -40px;color: #fff;width:100%">
+                <a href="https://beian.miit.gov.cn/">京ICP备19019954号</a>
             </div>
         </div>
     </div>
